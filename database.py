3499801f4c6c9bbb5d4fc95a195f05c792ef472e
NessaLiu <liu_vanessa@rocketmail.com>
2022-01-10 03:35:56 -0500

    Update database.py

--- database.py
+++ database.py
@@ -2,16 +2,22 @@ import mysql.connector
 import os
 
 
-url = os.environ['DATABASE_URL']
+#url = os.environ['DATABASE_URL']
 
+url = 'mysql://b933b28d9b839c:3cf3178c@us-cdbr-east-05.cleardb.net/heroku_f9c09781f41ef7b?reconnect=true'
 
-print(url)
+host = url.split('@')[1].split('/')[0] 
+user = url.split('/')[2].split(':')[0] 
+pwd = url.split(':')[2].split('@')[0] 
+database = url.split('/')[-1].split('?')[0]
+
 config = {
-    'host': url,
-    'user': None,
-    'password': None,
-    'database': None
+    'host': host,
+    'user': user,
+    'password': pwd,
+    'database': database
 }
+print(config)
 
 db = mysql.connector.connect(**config)
 cursor = db.cursor()
