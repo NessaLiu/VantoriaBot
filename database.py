027389d1bfa8271af92daad36dc492945013b400
NessaLiu <liu_vanessa@rocketmail.com>
2022-01-10 03:06:06 -0500

    Update database.py

--- database.py
+++ database.py
@@ -4,30 +4,16 @@ from urllib.parse import urlparse, uses_netloc
 import sys
 
 uses_netloc.append('mysql')
+DATABASES = {}
+url = urlparse(os.environ['DATABASE_URL'])
 
-try:
-    DATABASES = {}
+config = {
+    'NAME': url.path[1:],
+    'USER': url.username,
+    'PASSWORD': url.password,
+    'HOST': url.hostname,
+    'PORT': url.port,
+}
 
-    if 'DATABASE_URL' in os.environ:
-        url = urlparse(os.environ['DATABASE_URL'])
-
-        # Ensure default database exists.
-        DATABASES['default'] = DATABASES.get('default', {})
-
-        # Update with environment configuration.
-        DATABASES['default'].update({
-            'NAME': url.path[1:],
-            'USER': url.username,
-            'PASSWORD': url.password,
-            'HOST': url.hostname,
-            'PORT': url.port,
-        })
-except Exception:
-    print('Unexpected error:', sys.exc_info())
-
-print(DATABASES)
-
-db = mysql.connector.connect(DATABASES['default'])
+db = mysql.connector.connect(**config)
 cursor = db.cursor()
-
-
