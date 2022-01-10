5770477a6d8af5c47726b9b291942c97b0857fcd
NessaLiu <liu_vanessa@rocketmail.com>
2022-01-10 03:15:32 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,17 +1,15 @@
 import mysql.connector
 import os
-from urllib.parse import urlparse, uses_netloc
-import sys
 
-uses_netloc.append('mysql')
-DATABASES = {}
-url = urlparse(os.environ['DATABASE_URL'])
+
+url = os.environ['DATABASE_URL']
+
 
 config = {
-    'HOST': url.path[1:],
-    'USER': url.username,
-    'PASSWORD': url.password,
-    'PORT': url.port,
+    'host': url,
+    'user': None,
+    'password': None,
+    'database': None
 }
 
 db = mysql.connector.connect(**config)
